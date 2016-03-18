b92c9e4f675749cbca607124ce2579e253f61cca
ankraft <an.kraft@googlemail.com>
2016-03-18 15:06:01 +0100

    Abbreviation function, first version

--- SDT3PrintOneM2MSVG.py
+++ SDT3PrintOneM2MSVG.py
@@ -21,8 +21,8 @@ def print3OneM2MSVG(domain, directory, options):
 
 	lfile = options['licensefile']
 	if lfile != None:
-	    with open(lfile, 'rt') as f:
-	    	headerText = f.read()
+		with open(lfile, 'rt') as f:
+			headerText = f.read()
 
 	# Create package path and make directories
 
@@ -321,4 +321,79 @@ def sanitizeName(name, isClass):
 def sanitizePackage(package):
 	result = package.replace('/', '.')
 	return result
+
+def soundex(name, len=4):
+	""" soundex module conforming to Knuth's algorithm
+		implementation 2000-12-24 by Gregory Jorgensen
+		public domain
+		"""
+
+	# digits holds the soundex values for the alphabet
+	digits = '01230120022455012623010202'
+	# alternative: digits = '01230120002055012623010202'
+	sndx = ''
+	fc = ''
+
+	# translate alpha chars in name to soundex digits
+	for c in name.upper():
+		if c.isalpha():
+			if not fc: fc = c   # remember first letter
+			d = digits[ord(c)-ord('A')]
+			# duplicate consecutive soundex digits are skipped
+			if not sndx or (d != sndx[-1]):
+				sndx += d
+
+	# replace first digit with first alpha character
+	sndx = fc + sndx[1:]
+
+	# remove all 0s from the soundex code
+	sndx = sndx.replace('0','')
+
+	# return soundex code padded to len characters
+	return (sndx + (len * '0'))[:len]
+
+
+# experimental abreviation function. Move later
+
+def abbreviate(name, length=5):
+	l = len(name)
+	if l <= length:
+		result = name
+		while l < length:
+			for i in range(length - l):
+				result += result[i]
+			l = len(result)
+		return result.upper()
+	
+	# First char
+	result  = name[0]
+	
+	# Last char
+	result += name[-1]
+
+	if len(result) < length:
+		mask = name[1:l-1]
+		# Camel cases chars
+		camels = ''
+		for i in range(1,l-1):
+			c = name[i]
+			if c.isupper():
+				camels += c
+				mask = mask[:i-1] + mask[i:]
+		result = result[:1] + camels + result[1:]
+
+		# Fill with remaining chars of the mask, starting from the back
+		lm = len(mask)
+		for i in range(0, length - len(result)):
+			pos = i + 1
+			result = result[:pos] + mask[i] + result[pos:] 
+
+	return result[:length].upper()
+
+
+
+
+
+
+
 
